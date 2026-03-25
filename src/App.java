3f47d5bf4f874fc9b6be86ba0e982a57a2de7b72
intisaar02a-pixel <intisaar02a@gmail.com>
2026-03-25 15:41:35 +0100

    Update App.java
    Handle choice 2 using sub()

--- src/App.java
+++ src/App.java
@@ -5,5 +5,6 @@ public class App {
         System.out.println("Your choice: ");
         String choice = new java.util.Scanner(System.in).nextLine();
         if ("1".equals(choice)) System.out.println(Calculator.add(0,0));
+        if ("2".equals(choice)) System.out.println(Calculator.sub(a, b));
     }
 }
