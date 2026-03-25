96e5dc20a5a141d569bdcf810fcddda91de58941
noni <racilzanganeh@gmail.com>
2026-03-25 15:40:32 +0100

    Handle choice 3 using mul()

--- src/App.java
+++ src/App.java
@@ -6,4 +6,5 @@ public class App {
         String choice = new java.util.Scanner(System.in).nextLine();
         if ("1".equals(choice)) System.out.println(Calculator.add(0,0));
     }
+    if("3".equals(choice)) System.out.println(Calculator.mul(a,b));
 }
