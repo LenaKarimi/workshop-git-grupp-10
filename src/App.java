f0e0e4ea33885c0eba8a2613562bd06208d77373
LenaKarimi <lena.karimi@hotmail.com>
2026-03-25 15:57:43 +0100

    Read operand a and b

--- src/App.java
+++ src/App.java
@@ -5,6 +5,8 @@ public class App {
         System.out.println("Your choice: ");
         String choice = new java.util.Scanner(System.in).nextLine();
         if ("1".equals(choice)) System.out.println(Calculator.add(0,0));
+        Double a = Double.parseDouble(new java.util.Scanner(System.in).nextLine());
+        Double b = Double.parseDouble(new java.util.Scanner(System.in).nextLine());
         if ("2".equals(choice)) System.out.println(Calculator.sub(a, b));
     }
     if("3".equals(choice)) System.out.println(Calculator.mul(a,b));
